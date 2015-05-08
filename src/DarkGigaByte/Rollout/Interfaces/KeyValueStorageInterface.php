c4b9a622ab1032ee8a79e60050e101759c4a0c6a
Pascal Schneider <darkgigabyte@googlemail.com>
2015-05-09 00:50:59 +0200

    corrected phpdoc blocks and added first storage (arraystorage) as an example to make your own storages

--- src/DarkGigaByte/Rollout/Interfaces/KeyValueStorageInterface.php
+++ src/DarkGigaByte/Rollout/Interfaces/KeyValueStorageInterface.php
@@ -10,15 +10,19 @@ interface KeyValueStorageInterface
 {
 
     /**
+     * Gets a value from a key in the storage
+     *
      * @param $key
-     * @return mixed
+     * @return string|null
      */
     public function get($key);
 
     /**
+     * Sets a key in the storage to a value
+     *
      * @param $key
      * @param $value
-     * @return string|null
+     * @return KeyValueStorageInterface
      */
     public function set($key, $value);
 }
---
+++ src/DarkGigaByte/Rollout/Storage/ArrayStorage.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace DarkGigaByte\Rollout\Storage;
+
+
+use DarkGigaByte\Rollout\Interfaces\KeyValueStorageInterface;
+
+/**
+ * Class ArrayStorage
+ * @package DarkGigaByte\Rollout\Storage
+ */
+class ArrayStorage implements KeyValueStorageInterface
+{
+    /**
+     * @var array
+     */
+    protected $storage = array();
+
+    /**
+     * Gets a value from a key in the storage
+     *
+     * @param $key
+     * @return string|null
+     */
+    public function get($key)
+    {
+        $storageValue = null;
+        if (true === array_key_exists($key, $this->storage)) {
+            $storageValue = $this->storage[$key];
+        }
+        return $storageValue;
+    }
+
+    /**
+     * Sets a key in the storage to a value
+     *
+     * @param $key
+     * @param $value
+     * @return KeyValueStorageInterface
+     */
+    public function set($key, $value)
+    {
+        $this->storage[$key] = $value;
+        return $this;
+    }
+
+} 
